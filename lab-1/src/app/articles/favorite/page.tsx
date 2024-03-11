93827de06fe1bc83254b5c966ce24a1e874a6393
Олександр Пархоменко <42373411+senior-cherry@users.noreply.github.com>
2024-03-11 14:40:26 +0200

    articles/favorite get by id

--- lab-1/src/app/articles/favorite/page.tsx
+++ lab-1/src/app/articles/favorite/page.tsx
@@ -1,9 +1,35 @@
+'use client';
+import {useState, useEffect} from "react";
 import ArticlesLayout from "@/app/components/ArticlesLayout";
 
 const Favorite = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        let posts = [];
+        const fetchData = async () => {
+            try {
+                for (let i = 1; i < 4; i++) {
+                    const res = await fetch("https://jsonplaceholder.typicode.com/posts/" + i)
+                    const apiData = await res.json();
+                    posts.push(apiData);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+            setData(posts);
+        }
+
+        fetchData();
+    }, [])
     return (
         <ArticlesLayout>
-            <h1>Favorite</h1>
+            <p>{data.map((post) => {
+                return <div className="post">
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </div>
+            })}</p>
         </ArticlesLayout>
     );
 }
